fca38a91498c45132cb047ec9e0f3ffc19259954
Angel Psiakis <angel@mml.rocks>
2024-07-16 21:56:01 +0100

    Add prometheus & graphana

--- node-app/index.js
+++ node-app/index.js
@@ -1,9 +1,29 @@
 import express from "express";
+import promClient from "prom-client";
 
 const app = express();
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+
+// Collect default metrics
+collectDefaultMetrics();
+
+// Create a custom metric
+const httpRequestsTotal = new promClient.Counter({
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "path"],
+});
 
 app.get("/", (req, res) => {
+  // Increment the custom metric
+  httpRequestsTotal.inc({ method: req.method, path: req.path });
+
   res.send("Hello World");
+});
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", promClient.register.contentType);
+  res.end(await promClient.register.metrics());
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
